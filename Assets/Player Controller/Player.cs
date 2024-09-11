8d46dfeb1b4d519d162a0a540ae31028a9bcbcda
Molflyn <143250086+Molflyn@users.noreply.github.com>
2024-09-12 00:21:51 +0800

    Almost figured out movement

--- Assets/Player Controller/Player.cs
+++ Assets/Player Controller/Player.cs
@@ -1,33 +1,56 @@
 using Godot;
 using System;
+using System.Numerics;
+using System.Security.AccessControl;
+using Vector2 = Godot.Vector2;
+using Vector3 = Godot.Vector3;
 
 public partial class Player : CharacterBody3D
 {
-	public const float Speed = 5.0f;
-	public const float JumpVelocity = 4.5f;
+	[ExportGroup("Ground Movement")]
+	[Export] public float CrouchSpeed = 4f;
+	[Export] public float MoveSpeed = 6f;
+	[Export] public float SprintMultiplier = 1.2f;
+	[Export] public float Acceleration = 15f;
+    [Export] public float RollSpeed = 10f;
+    private bool _isCrouching = false;
+    private Vector2 _inputDirection;
+    private Vector3 _moveDirection = Vector3.Zero;
+    private Vector3 _targetVelocity;
+
+    private void HandleInput()
+    {
+		_inputDirection = Input.GetVector("move_forward", "move_back", "move_left", "move_right");
+		_moveDirection = (Transform.Basis * new Vector3(_inputDirection.X, 0, _inputDirection.Y).Normalized());
+		_targetVelocity = _moveDirection * MoveSpeed;
+    }
+
+    private void Crouch()
+    {
+	    if (Input.IsActionJustPressed("crouch") && _isCrouching == false)
+	    {
+		    _isCrouching = true;
+		    _targetVelocity = _moveDirection * CrouchSpeed;
+	    }
+	    else if (Input.IsActionJustPressed("crouch") && _isCrouching == true)
+	    {
+		    _isCrouching = false;
+		    _targetVelocity = _moveDirection * MoveSpeed;
+	    }
+    }
+    
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		HandleInput();
+		Crouch();
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector3 velocity = Velocity;
-		// Add the gravity.
-		if (!IsOnFloor())
-		{
-			velocity += GetGravity() * (float)delta;
-		}
-	
-		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-		if (direction != Vector3.Zero)
-		{
-			velocity.X = direction.X * Speed;
-			velocity.Z = direction.Z * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
-		}
-
+		velocity.X = (float)Mathf.Lerp(velocity.X, _targetVelocity.Z, Acceleration * delta);
+		velocity.Z = (float)Mathf.Lerp(velocity.Z, _targetVelocity.X, Acceleration * delta);
+		
 		Velocity = velocity;
 		MoveAndSlide();
 	}
